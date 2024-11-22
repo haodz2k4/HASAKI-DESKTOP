cf073015769f469f309806a509a2425a7cbcc19e
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-11-22 08:39:15 +0700

    Login template and update other

--- src/main/app.js
+++ src/main/app.js
@@ -1,31 +1,31 @@
-const {app, BrowserWindow } = require("electron");
+const path = require("path"); 
+const { app, BrowserWindow } = require("electron");
 
 let mainWindow;
 
-
-app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        width: 1700,
-        height: 1400,
-        webPreferences: {
-          nodeIntegration: true,
-        },
-      });
-    
-      mainWindow.loadFile('../renderer/views/home/home.html'); 
-      mainWindow.on('closed', () => {
-        mainWindow = null;
-      });
-})
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
-});
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    width: 1500,
+    height: 1200,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadFile(path.join(__dirname, "../renderer/views/auth/login.html"));
   
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
